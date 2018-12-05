16c38f8a7e8f6609a32254cd84d28ee8ae910db6
Ashish Gupta <akvns152@gmail.com>
2018-12-05 15:12:10 +0530

    code upgraded

--- js/app.js
+++ js/app.js
@@ -1,40 +1,9 @@
-
-    $("#home").click(function()
-    {
-        $(document).ready(function(){
-
-        $("#main-view").load("new/index.html");
-        });
-    });
-
-
-    $("#profile").click(function()
-    {
-        $(document).ready(function(){
-        $("#main-view").load("pages/profile.html");
-    });
+$('#home').click(function(){
+	$('#main-view').load('./pages/home.html');
 });
-
-
-    $("#skills").click(function()
-    {
-        $(document).ready(function(){
-        $("#main-view").load("pages/skills.html");
-    });
+$('#profile').click(function(){
+	$('#main-view').load('./pages/profile.html');
 });
-
-
-    $("#work").click(function()
-    {
-        $(document).ready(function(){
-        $("#main-view").load("pages/work.html");
-    });
+$('#work').click(function(){
+	$('#main-view').load('./pages/work.html');
 });
-
-    $("#contact").click(function()
-    {
-        $(document).ready(function(){
-        $("#main-view").load("pages/contact.html");
-    });
-});
-
